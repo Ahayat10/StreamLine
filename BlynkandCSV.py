7598a1c2a266b7f6e9d012e85808134fc35a0e1e
Amna Hayat <70821212+Ahayat10@users.noreply.github.com>
2024-04-16 01:13:04 -0400

    Update BlynkandCSV.py

--- BlynkandCSV.py
+++ BlynkandCSV.py
@@ -2,7 +2,7 @@
 import time
 from BlynkTimer import BlynkTimer
 import BlynkLib
-import csv 
+import csv
 from datetime import datetime
 
 # Import Blinka Libraries
@@ -13,7 +13,7 @@ import board
 # Import RFM9x
 import adafruit_rfm9x
 
-BLYNK_AUTH_TOKEN = 'YOURAUTENTICATIONTOKEN'
+BLYNK_AUTH_TOKEN = 'YOURAUTHENTICATIONTOKEN'
 
 # Initialize Blynk
 blynk = BlynkLib.Blynk(BLYNK_AUTH_TOKEN)
@@ -34,9 +34,9 @@ timer = BlynkTimer()
 
 @blynk.on("connected")
 def blynk_connected():
-    print("Raspberry Pi Connected to New Blynk") 
+    print("Raspberry Pi Connected to New Blynk")
     time.sleep(2)
-    
+
 @blynk.on("V0")
 def v0_write_handler(value):
     message = None
@@ -48,8 +48,8 @@ def v0_write_handler(value):
         message = "ON"
         print('Received ON')
         sendDataEsp(message)
-    # print("SENT TO ESP")
-    
+        # print("SENT TO ESP")
+
 def sendApp():
     humidity, temperature, moisture = receiveData()
     #print("app" , humidity, temperature, moisture, valve_state)
@@ -67,53 +67,60 @@ def sendDataEsp(message, repeat=10):
             print(f"Failed to send {message}")
 
 def receiveData():
-    # global send_counter  # Declare send_counter as a global variable
-    humidity = None # Initialize humidity variable as None
-    temperature = None # Initialize temperature variable as None
+    # global send_counter # Declare send_counter as a global variable
+    humidity = None  # Initialize humidity variable as None
+    temperature = None  # Initialize temperature variable as None
     moisture = None  # Initialize moisture variable as None
-    #valve_state = None  # Initialize valve_state variable as None
-    
+    x = None
+    #valve_state = None # Initialize valve_state variable as None
+
     packet = rfm9x.receive()
     if packet is not None:
         try:
             # Try to decode the packet data as UTF-8
             packet_text = packet.decode("utf-8")
             print("Received LoRa packet:", packet_text)
-
             # Parse the received LoRa packet
             key_value_pairs = packet_text.split(',')
-            print("keyValue:" , key_value_pairs)
+            print("keyValue:", key_value_pairs)
             parsed_data = {}
-
             for pair in key_value_pairs:
                 parts = pair.strip().split(':', 1)  # Split at the first colon
-                #print("parts:" , parts)
+                print("parts:", parts)
                 #print("length:" , len(parts))
                 if len(parts) == 2:
                     key, value = parts[0].strip(), parts[1].strip()
                     parsed_data[key] = value
-            
+
             if 'dity' in parsed_data:
                 humidity = parsed_data['dity']
-                
+
             if 'Temp' in parsed_data:
                 temperature = parsed_data['Temp']
-
             if 'Moisture Level' in parsed_data:
                 moisture = float(parsed_data['Moisture Level'].rstrip('%'))
 
-            print("humidity:" , humidity)
-            print("Temp: " , temperature)
+            if 'x' in parsed_data:
+                xyz_values = parsed_data['x'].split()
+                x = float(xyz_values[0])
+                # if 'Valve State' in parsed_data:
+                # valve_state = parsed_data['Valve State']
+
+            print("humidity:", humidity)
+            print("Temp: ", temperature)
             print("Moisture:", moisture)
-            
+            print("x:", x)
+            # print("valve_state:", valve_state)
+            #blynk.virtual_write(4, moisture)
+            if moisture is not None and (moisture >= 60):
+                sendDataEsp("OFF")
         except UnicodeDecodeError:
             print("Failed to decode the packet as UTF-8")
-    
+
     return humidity, temperature, moisture
 
 send_counter = 0  # Packet counter
 filename = "SensorData.csv"
-
 with open(filename, mode='a', newline='') as csvfile:
     csv_writer = csv.writer(csvfile)
     if csvfile.tell() == 0:
@@ -123,15 +130,11 @@ while True:
     blynk.run()
     humidity, temperature, moisture = receiveData()
     sendApp()
-
-
     if humidity is not None and temperature is not None and moisture is not None:
         # Get the current timestamp
         timestamp = time.strftime("%Y-%m-%d %H:%M:%S")
-
         # Write data with timestamp to the CSV file
         with open(filename, mode='a', newline='') as csvfile:
             csv_writer = csv.writer(csvfile)
             csv_writer.writerow([timestamp, humidity, temperature, moisture])
-
-        time.sleep(2)  # Adjust sleep duration based on your requirements
+    time.sleep(2)  # Adjust sleep duration based on your requirements
